06af2b4a5f8f7dca6f963f028984d7cd1d73e67f
Ninon <ninon.jordao@outlook.fr>
2020-08-21 16:51:20 +0200

    ajout dossier utils

---
+++ app/src/main/java/com/example/devmobile_efficom/utils/MovieClickListener.java
@@ -0,0 +1,9 @@
+package com.example.devmobile_efficom.utils;
+
+import com.example.devmobile_efficom.models.Movie;
+
+public interface MovieClickListener {
+
+    void onMovieClick(Movie movie);
+
+}
