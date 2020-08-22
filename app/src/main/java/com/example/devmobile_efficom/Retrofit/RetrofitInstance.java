289ba6137b9dfaae342dfaa9dc116e0f2edf5f64
Ninon <ninon.jordao@outlook.fr>
2020-08-22 17:02:25 +0200

    ajout du service

--- app/src/main/java/com/example/devmobile_efficom/Retrofit/RetrofitInstance.java
+++ app/src/main/java/com/example/devmobile_efficom/Retrofit/RetrofitInstance.java
@@ -5,19 +5,11 @@ import com.example.devmobile_efficom.Constantes.Constantes;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-/**
- * Created by Evert Moreno on 24-01-2018.
- */
+
 public class RetrofitInstance {
 
     private static Retrofit retrofit = null;
 
-
-    /**
-     * Gets client.
-     *
-     * @return the client
-     */
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
---
+++ app/src/main/java/com/example/devmobile_efficom/Services/MovieDataService.java
@@ -0,0 +1,18 @@
+package com.example.devmobile_efficom.Services;
+
+import com.example.devmobile_efficom.models.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Service qui va permettre de recuperer les donnees de l'api 
+ */
+public interface MovieDataService {
+
+    @GET("movie/popular")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+
+}
